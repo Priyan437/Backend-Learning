c752479c1072320a5cdc8851ff97d3914161e6ae
Priyanshu <hopestolive678@gmail.com>
2026-08-21 16:09:33 +0530

    Basics of Java Script and program for finding the area of the circle

---
+++ index.js
@@ -0,0 +1,81 @@
+// console.log(`Hi I am there in Server`);
+// console.log(`Debugg it brol`);
+
+// window.alert("Hi I am here");
+// window.alert("How is the Day");
+
+// document.getElementById("hi").textContent = 'Hello I am there in the DOM';
+// document.getElementById("para").textContent = 'HI erase all';
+
+// let x;
+// x = 100;
+
+// console.log(x);
+
+// let y = 200.3659;
+// console.log(`Your value of Product is ${y}`);
+
+
+// let firstName = 'Priyanshu';
+// console.log(typeof firstName);
+
+// let Online = true;
+// console.log(typeof Online);
+
+// let fullName = 'Priyanshu Kumar Thakur';
+// let Age = 21;
+// let isStudent = true;
+
+// document.getElementById("p1").textContent = `His name is ${fullName}`;
+// document.getElementById("p2").textContent = `His Age is ${Age}`;
+// document.getElementById("p3").textContent = `Is Student: ${isStudent}`;
+
+/* Augmented Assignment Operators */
+
+// let x = 120;
+// x += 10;
+// x -= 100;
+// x *= 2;
+// x **= 2;
+
+/* 
+Operator Precedence
+    
+    Brackets
+    Exponents
+    Multiplications, Divisions, Modulo
+    Addition, Subtractions 
+    
+    */
+
+
+// // How to Accept User Input in the Java Script
+
+// /* 1. Easy Way = Window Prompt.
+//    2. Hard Way = HTML Text Box 
+// */
+
+// let username;
+//1.
+// username = window.prompt("Enter your Good Name");
+// console.log(username);
+
+//2.
+
+// let username;
+
+// document.getElementById("b").onclick = function(){
+//     username = document.getElementById("Inputtext").value;
+//     document.getElementById("hmm").textContent = `Hello Beloved ${username}`;
+//     console.log(username);
+// }
+
+/* Code for finding the Are of the Circle
+*/
+
+     let d;
+     document.getElementById("b").onclick = function(){
+        d = document.getElementById("Dia").value;
+        let area = (3.14 * d**2)/4;
+        document.getElementById("answer").textContent = `area of circle is ${area}`;
+     };
